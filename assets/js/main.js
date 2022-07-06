947c428e74baad234e929609d07b6fcbb4e8a8e8
kishankkeshri <kishankkeshri@gmail.com>
2022-07-06 20:38:28 +0530

    p

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,17 +57,17 @@ sr.reveal('.home__img', {origin:'right', delay: 400})
 /*SCROLL ABOUT*/
 sr.reveal('.about__img', {delay: 500})
 sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__profession', {delay: 100})
 sr.reveal('.about__text', {delay: 500})
 sr.reveal('.about__social-icon', {delay: 600, interval: 200})
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__img', {delay: 100})
 
 /*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
+sr.reveal('.portfolio__img', {interval: 50})
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
